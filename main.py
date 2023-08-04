18af04cae5ae10c93e8ec5045dd45e100861c6d6
Daniele Lupo <lupodaniele94@gmail.com>
2023-08-04 21:37:33 +0200

    Esteso il grafico a tutti i dati contenuti nel csv

--- main.py
+++ main.py
@@ -323,79 +323,108 @@ async def readFromNodered(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
 
 
 def createChart():
-    with open("./temperature.csv", "r", encoding="utf-8") as file:
-        lines = file.readlines()[-10000:]
-        filteredLines = []
-        for l in lines:
-            if len(l.strip()) > 0:
-                filteredLines.append(l.strip())
+    daily_data = {}
 
-        xValues = []
-        yTemp = []
-        yHum = []
-        maxTemp = 0
-        minTemp = 100
-        maxHum = 0
-        minHum = 200
-        for l in filteredLines:
+    with (open("./temperature.csv", "r")) as f:
+        allLines = f.readlines()
+        allLines.reverse()
+        for l in allLines:
+            l = l.strip()
+            l = l.replace("\x00", "")
+            if len(l) < 1:
+                continue
             values = l.split(",")
-            ms = float(values[1])
-            xValues.append(datetime.datetime.fromtimestamp(ms/1000.0))
-            yTemp.append(float(values[0]))
-            yHum.append(float(values[2]))
+            temperature = float(values[0])
+            humidity_value = float(values[2])
+            timestamp_ms = int(values[1])
 
-            maxTemp = max(yTemp)
-            maxHum = max(yHum)
+            timestamp_sec = timestamp_ms / 1000.0
+            date_object = datetime.fromtimestamp(timestamp_sec)
+            date_only = date_object.date()
 
-            minTemp = min(yTemp)
-            minHum = min(yHum)
+            if date_only in daily_data:
+                # If the date already exists, update the daily values
+                daily_data[date_only]['temperature'].append(temperature)
+                daily_data[date_only]['humidity'].append(humidity_value)
+            else:
+                # If the date doesn't exist, create a new entry
+                daily_data[date_only] = {
+                    'temperature': [temperature],
+                    'humidity': [humidity_value]
+                }
 
-        plt.figure(figsize=(20, 12))  # Set the figure size (adjust as needed)
+    daily_averages = {}
+    for date, data in daily_data.items():
+        avg_temperature = sum(data['temperature']) / len(data['temperature'])
+        avg_humidity = sum(data['humidity']) / len(data['humidity'])
+        daily_averages[date] = {
+            'average_temperature': avg_temperature,
+            'average_humidity': avg_humidity
+        }
 
-        plt.plot(xValues, yTemp, label='Temperature')
-        plt.plot(xValues, yHum, label='Humidity')
+    xValues = []
+    yTemp = []
+    yHum = []
+    # Print the daily averages
+    for date, data in daily_averages.items():
+        print(
+            f"Date: {date}, Average Temperature: {data['average_temperature']:.2f}, Average Humidity: {data['average_humidity']:.2f}")
+        xValues.append(date)
+        yTemp.append(float(f"{data['average_temperature']:.2f}"))
+        yHum.append(float(f"{data['average_humidity']:.2f}"))
 
-        textPosition = len(xValues)/2 + len(xValues)/4
-        # print(f'{textPosition}')
+    maxTemp = max(yTemp)
+    maxHum = max(yHum)
 
-        textPosition = xValues[int(textPosition)]
+    minTemp = min(yTemp)
+    minHum = min(yHum)
 
-        plt.axhline(maxTemp, color='red', linestyle='--',
-                    label=f'Max temperature {maxTemp}')
+    plt.figure(figsize=(20, 12))  # Set the figure size (adjust as needed)
 
-        plt.text(textPosition, maxTemp, f'Max temperature = {maxTemp}°C', color='red',
-                 fontsize=10, ha='right', va='bottom')
+    plt.plot(xValues, yTemp, label='Temperature')
+    plt.plot(xValues, yHum, label='Humidity')
 
-        plt.axhline(minTemp, color='darkorange', linestyle='--',
-                    label=f'Min temperature {minTemp}')
+    textPosition = len(xValues)/2 + len(xValues)/4
+    # print(f'{textPosition}')
 
-        plt.text(textPosition, minTemp, f'Min temperature = {minTemp}°C', color='darkorange',
-                 fontsize=10, ha='right', va='bottom')
+    textPosition = xValues[int(textPosition)]
 
-        plt.axhline(maxHum, color='blue', linestyle='--',
-                    label=f'Max humidity {maxHum}')
+    plt.axhline(maxTemp, color='red', linestyle='--',
+                label=f'Max temperature {maxTemp}')
 
-        plt.text(textPosition, maxHum, f'Max humidity = {maxHum}%', color='blue',
-                 fontsize=10, ha='right', va='bottom')
+    plt.text(textPosition, maxTemp, f'Max temperature = {maxTemp}°C', color='red',
+             fontsize=10, ha='right', va='bottom')
 
-        plt.axhline(minHum, color='deepskyblue', linestyle='--',
-                    label=f'Min humidity {minHum}')
+    plt.axhline(minTemp, color='darkorange', linestyle='--',
+                label=f'Min temperature {minTemp}')
 
-        plt.text(textPosition, minHum, f'Min humidity = {minHum}%', color='deepskyblue',
-                 fontsize=10, ha='right', va='bottom')
+    plt.text(textPosition, minTemp, f'Min temperature = {minTemp}°C', color='darkorange',
+             fontsize=10, ha='right', va='bottom')
 
-        plt.xlabel('Time')
-        plt.ylabel('Temperature and Humidity')
-        plt.title('Temperature and Humidity over time')
-        plt.legend()
+    plt.axhline(maxHum, color='blue', linestyle='--',
+                label=f'Max humidity {maxHum}')
 
-        buf = io.BytesIO()
-        plt.savefig(buf, format='png')
-        buf.seek(0)
+    plt.text(textPosition, maxHum, f'Max humidity = {maxHum}%', color='blue',
+             fontsize=10, ha='right', va='bottom')
 
-        plt.close()
+    plt.axhline(minHum, color='deepskyblue', linestyle='--',
+                label=f'Min humidity {minHum}')
 
-        return buf
+    plt.text(textPosition, minHum, f'Min humidity = {minHum}%', color='deepskyblue',
+             fontsize=10, ha='right', va='bottom')
+
+    plt.xlabel('Time')
+    plt.ylabel('Temperature and Humidity')
+    plt.title('Temperature and Humidity over time')
+    plt.legend()
+
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+
+    plt.close()
+
+    return buf
 
 
 async def getchart(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
